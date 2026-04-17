8d077971da2d7f133dc9f23ed322a3cdbe35258d
Kyle Cuthbert <Kyle.Cuthbert@cardiff.gov.uk>
2026-04-17 11:22:25 +0100

    Add MCTPopupExtensions for MAUI CommunityToolkit popups
    Introduce Plugin.Maui.SmartNavigation.MCTPopupExtensions project to support dependency-resolved CommunityToolkit.Maui popups. Refactor NavigationExtensions to resolve IView instead of just Page, enabling support for both Page and Popup types. Add extension methods for INavigation to push popups with resolved dependencies. Update InternalsVisibleTo and package references for compatibility and improved dependency management. Remove conditional from ProjectReference in MopupsExtensions.

---
+++ src/Plugin.Maui.SmartNavigation.MCTPopupExtensions/MCTPopupExtensions.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Extensions;
+using CommunityToolkit.Maui.Views;
+using Plugin.Maui.SmartNavigation.Extensions;
+
+namespace Plugin.Maui.SmartNavigation;
+
+public static class MCTPopupExtensions
+{
+    public static Task<IPopupResult> PushAsync<T>(this INavigation navigation, IPopupOptions? options, CancellationToken cancellationToken) where T : Popup
+    {
+        var popup = NavigationExtensions.ResolvePage<T>();
+
+        var test = popup as Popup
+             ?? throw new ArgumentException("Could not resolve popup page");
+
+        return navigation.ShowPopupAsync(test, options, cancellationToken);
+    }
+    public static Task<IPopupResult> PushAsync<T>(this INavigation navigation, IPopupOptions? options, CancellationToken cancellationToken, params object[] parameters) where T : Popup
+    {
+        var popup = NavigationExtensions.ResolvePage<T>(parameters) as Popup
+           ?? throw new ArgumentException("Could not resolve popup page");
+
+        return navigation.ShowPopupAsync(popup, options, cancellationToken);
+    }
+    public static Task<IPopupResult<TResult>> PushAsync<T, TResult>(this INavigation navigation, IPopupOptions? options, CancellationToken cancellationToken) where T : Page
+    {
+        var popup = NavigationExtensions.ResolvePage<T>() as Popup
+          ?? throw new ArgumentException("Could not resolve popup page");
+
+        return navigation.ShowPopupAsync<TResult>(popup, options, cancellationToken);
+    }
+    //public static Task<IPopupResult<TResult>> PushAsync<T, TResult>(this IPopupService popupService, IPopupOptions? options, CancellationToken cancellationToken, params object[] parameters) where T : Page
+    //{
+    //    var popup = NavigationExtensions.ResolvePage<T>(parameters) as Page
+    //      ?? throw new ArgumentException("Could not resolve popup page");
+
+    //    return popupService.ShowPopupAsync<T, TResult>(popup, options, cancellationToken);
+    //}
+}
