a5a75e0862a9b4ec8f4fa0d34bda6bc9fcc2c94c
Kyle Cuthbert <kylecuthbert32@gmail.com>
2026-04-20 13:49:35 +0100

    Add Mct Popups feature with sample message popup
    Registered MctPopupsMainPage and MessagePopup in the project. Added a "Mct Popups" button to MainPage and navigation command in MainViewModel. Implemented MessagePopup with data binding and submit button. Updated BasePopup XAML for correct namespace and class usage. Introduces CommunityToolkit.Maui-based popup system.

---
+++ src/DemoProject/Popups/Pages/Mct/MctPopupsMainPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace DemoProject.Popups.Pages.Mct;
+
+public partial class MctPopupsMainPage : ContentPage
+{
+	public MctPopupsMainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnMessagePopup_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
